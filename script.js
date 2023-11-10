2c1633edf651814f838e7ca7c32bd92fa3ade313
Stephen Kalogridis <stephen.kalogridis@gmail.com>
2023-11-09 20:22:48 -0500

    Tweak onload functionality and new game behavior

--- script.js
+++ script.js
@@ -5,57 +5,85 @@
 // game mode
 // game dificulty (if mode == vs CPU)
 
-// GAME LOGIC
+// GLOBAL VARIABLES
+const gameBoard = document.querySelector(".game");
+const newGame = document.querySelector(".newGame");
+let tileArr = [];
+let playerArr = [];
 
-function createPlayer(name, role) {
-  // player factory
-  // obj props
-  // obj name
-  // obj role (x or y)
-  // score
-  let role = "X";
-  let score = 0;
-
-  return { name, role, score };
+// create player factory
+function createPlayers() {
+  for (let i = 1; i < 3; i++) {
+    const player = {
+      name: "player" + i,
+      role: i == 1 ? "X" : "O",
+      nameDisplay: document.querySelector(`#p${i}-name`),
+      scoreDisplay: document.querySelector(`#p${i}-score`),
+    };
+    playerArr.push(player);
+  }
 }
 
-const gameBoard = document.querySelector(".game");
-
-window.onload = function () {
-  let tileArr = [];
-
-  function createTile(name, value) {
+// create board
+function createBoard() {
+  for (let i = 1; i <= 9; i++) {
+    // make tile obj
+    const name = "tile" + i;
+    const value = "";
     const newObj = { name, value };
     tileArr.push(newObj);
+    // make tile divs
     const newTile = document.createElement("div");
     newTile.id = `${name}`;
+    newTile.classList.add("tile");
     newTile.textContent = newObj.value;
+    // add functionality to tiles
+    newTile.addEventListener("mousedown", () => {
+      if (newTile.textContent == "") {
+        newTile.textContent = playerArr[0].role;
+        checkGame();
+        playerArr.reverse();
+      }
+    });
+    // add tiles to gameboard
     gameBoard.appendChild(newTile);
   }
+}
 
-  for (let i = 1; i <= 9; i++) {
-    createTile("tile" + i, "");
+function checkGame() {}
+// clears board
+function clearBoard() {
+  tileArr = [];
+  while (gameBoard.firstChild) {
+    gameBoard.removeChild(gameBoard.firstChild);
   }
+}
+
+// make players on window load
+window.onload = () => {
+  clearBoard();
+  createPlayers();
+  createBoard();
 };
 
-// game board obj
-// tiles : [tile1, tile2, ...]
-//
+// add functionality to new game button
+newGame.addEventListener("mousedown", () => {
+  // create 9 tiles
+  clearBoard();
+  createBoard();
+});
 
 // GAME OPTIONS MODULE
 // module for changing game functinoality
 // game difficulty
 // game mode
-
 // GAMEPLAY MODULE
-//
-
-const gameOptions = (function () {
-  // set game type func
-  // 1v1
-  // easy
-  // hard
-  // set playerRole(player)
-  // X
-  // O
-})();
+// const gameOptions = (function () {
+//   // set game type func
+//   // 1v1
+//   // easy
+//   // hard
+//   // set playerRole(player)
+//   // X
+//   // O
+// })();
